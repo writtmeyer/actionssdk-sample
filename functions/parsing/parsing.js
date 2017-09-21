64c4d589f00c759734466b84021afd64fc7f70a8
Wolfram Rittmeyer <wolfram.rittmeyer@grokkingandroid.com>
2017-09-21 11:54:14 +0200

    Improve parsing readability
    The actual test in line 6 checking for the availability
    of the html param is more concise an still better
    readable in the new version.
    
    The error message should the param not be available
    is also more telling than before.

--- functions/parsing/parsing.js
+++ functions/parsing/parsing.js
@@ -2,14 +2,14 @@
 
 const parser = require("cheerio");
 
-exports.getNextTransfers = function(html) {
-    if ((typeof html) == "undefined") {
-        throw Error('param html must not be empty');
+exports.getNextTransfers = function (html) {
+    if (!html) {
+        throw Error('param html must neither be null nor undefined');
     }
     let $ = parser.load(html);
     let transfers = [];
     let outerDivs = $('div.bgdark, div.bgwith, div.bgwhite');
-    outerDivs.each(function(index, element) {
+    outerDivs.each(function (index, element) {
         let line = $(this).find("div[class='line']").text();
         let direction = $(this).find("div[class='direction']").text();
         let time = $(this).find("div[class='time']").text();
